ffa8a93800d834e26c9aa252b3539e95b7469273
M.Aliraza <techniqueszack@gmail.com>
2024-01-02 23:49:00 +0500

    It is JS Assignment 2

---
+++ practice1.js
@@ -0,0 +1,27 @@
+//loops 
+/*for(let i = 1; i <= 100; i++) {
+    console.log("Hello World")
+}*/
+
+//table 10
+/*for(let i = 1 ; i <= 10; i++) {
+    console.log(10*i)
+}*/
+
+//if-else
+/*let a=6
+let b=0
+if (a>b) {
+    console.log("yes! a is greater than b")
+} else {
+    console.log("No! a is not greater than b")
+}*/
+
+//if else-if condition
+let a= 780
+let b = 1000
+if (a==1000) {
+    console.log("A+");
+} else if(a<=780) {
+    console.log("A");
+}
